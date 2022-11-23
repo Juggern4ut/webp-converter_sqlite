cf3e799121eed9b801bd7f09923e79f75ac1c0b3
Lukas Meier <luki-meier@outlook.com>
2022-11-23 16:46:52 +0200

    big performance improvement when going over folders with large amount of files that have all been converted

--- convert.py
+++ convert.py
@@ -5,16 +5,24 @@ import sys
 from subprocess import call
 import sqlite3
 import logging
-
+from pathlib import Path
 
 path = sys.argv[1]
 quality = sys.argv[2]
 logfile = sys.argv[3]
 
+viable_endings = [".png", ".jpg"]
+
 con = sqlite3.connect("logs.db")
 cur = con.cursor()
 
 results = {"new" : 0, "skipped" : 0, "changed" : 0, "missing" : 0, "quality" : 0}
+
+def getIndexOfTuple(l, index, value):
+    for pos,t in enumerate(l):
+        if t[index] == value:
+            return pos
+    return -1
 
 def setup_db(cur):
 
@@ -38,10 +46,20 @@ def convert_image(input: str, output: str, quality: int) -> None:
 
 def convert_folder(path):
     files = os.scandir(path)
+
+    if os.path.isdir(path):
+
+        query = f"SELECT path, file, timestamp, quality FROM convertion_times WHERE path = '{os.path.abspath(path)}\\'"
+        cur.execute(query)
+        res = cur.fetchall()
+
     for f in files:
         if f.is_dir():
             convert_folder(f)
         if f.is_file():
+
+            if not os.path.splitext(f)[1] in viable_endings:
+                continue 
 
             folder = os.path.abspath(f.path).replace(f.name, "")
 
@@ -50,27 +68,25 @@ def convert_folder(path):
             tmpParts = newFile.split(".")
             ending = tmpParts[len(tmpParts) - 1]
             newFile = newFile.replace(f".{ending}", ".webp")
-            timestamp = os.path.getmtime(f)
+            timestamp = int(os.path.getmtime(f))
+            
+            found_index = getIndexOfTuple(res, 1, f.name)
 
-            cur.execute(
-                f"SELECT path, file, timestamp, quality FROM convertion_times WHERE path = '{folder}' AND file='{f.name}' LIMIT 1"
-            )
-            res = cur.fetchone()
-            if res == None:
+            if found_index == -1:
                 logging.info(f"Converting new image {originalFile}")
                 cur.execute(
                     f"INSERT INTO convertion_times (path, file, timestamp, quality) VALUES ('{folder}', '{f.name}', '{timestamp}', {quality})"
                 )
                 convert_image(originalFile, newFile, quality)
                 results["new"] += 1
-            elif float(res[2]) < timestamp:
+            elif int(float(res[found_index][2])) < timestamp:
                 logging.info(f"Converting changed file {originalFile}")
                 cur.execute(
                     f"UPDATE convertion_times SET timestamp='{timestamp}', quality={quality} WHERE path = '{folder}' AND file='{f.name}'"
                 )
                 convert_image(originalFile, newFile, quality)
                 results["changed"] += 1
-            elif int(res[3]) != int(quality):
+            elif int(res[found_index][3]) != int(quality):
                 logging.info(f"Converting with new quality {originalFile}")
                 cur.execute(
                     f"UPDATE convertion_times SET timestamp='{timestamp}', quality={quality} WHERE path = '{folder}' AND file='{f.name}'"
@@ -86,6 +102,9 @@ def convert_folder(path):
                 results["missing"] += 1
             else:
                 results["skipped"] += 1
+            
+            if found_index != -1:
+                del res[found_index]
 
     con.commit()
 
